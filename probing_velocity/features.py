91c2ed0ba7e6d5d15ba837a1b93374c9d59579d6
Shuangge Wang <77814631+wshuangge@users.noreply.github.com>
2022-11-09 14:10:00 -0800

    Delete probing_velocity directory

--- probing_velocity/features.py
+++
@@ -1,9 +0,0 @@
-import utils
-
-
-class Features:
-    def __init__(self):
-        return
-
-    def weighted_sum(self, state, phi):
-        return (-(state[3] - phi[0])**2 - ((state[0]-state[2]) - phi[1])**2)/utils.sigma
